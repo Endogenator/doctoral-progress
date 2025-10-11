a4a954a5f1163dbda1fd6754a3884c333bfb7575
Brian Davis <brianruthvendavis@gmail.com>
2025-10-11 16:53:23 -0700

    Simplify Clerk auth components (no routing prop)

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,7 @@ import { SignIn } from '@clerk/nextjs'
 export default function SignInPage() {
   return (
     <div className="p-8">
-      <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+      <SignIn path="/sign-in" signUpUrl="/sign-up" />
     </div>
   )
 }
--- src/app/sign-up/[[...sign-up]]/page.tsx
+++ src/app/sign-up/[[...sign-up]]/page.tsx
@@ -4,7 +4,7 @@ import { SignUp } from '@clerk/nextjs'
 export default function SignUpPage() {
   return (
     <div className="p-8">
-      <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
+      <SignUp path="/sign-up" signInUrl="/sign-in" />
     </div>
   )
 }
